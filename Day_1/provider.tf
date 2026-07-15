72b8cabb3e1730a47079533602c9d45934714e95
Andre Araujo <andredearaujo2007@gmail.com>
2026-07-15 14:50:10 +0100

    Adding more one provider Virginia

--- Day_1/provider.tf
+++ Day_1/provider.tf
@@ -1,23 +1,28 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 5.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
 
   }
 
   #Here configuration backend in S3 bucket AWS
+  #backend é o nome do provider
   backend "s3" {
     bucket = "descomplicando-terraform-turma-20xx" #bucket s3 name in AWS
-    key    = "aula_backend" #name the file you want save it
-    region = "eu-west-2" # This is AWS region you are
+    key    = "aula_backend"                        #name the file you want save it
+    region = "eu-west-2"                           # This is AWS region you are
   }
 
 }
 
-#Here this provider your region
-provider"aws"{
-region="eu-west-2"
+#Here this provider your region London
+provider "aws" {
+  region = "eu-west-2"
 }
 
+#Here this provider your region Virginia USA
+provider "aws" {
+  region = "us-east-1"
+}
