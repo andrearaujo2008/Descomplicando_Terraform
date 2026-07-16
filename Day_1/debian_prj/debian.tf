251e159542346cd4ae24e8e00d9cc56428bca141
Andre Araujo <andredearaujo2007@gmail.com>
2026-07-16 09:22:37 +0100

    Estudos sobre variaveis

--- Day_1/debian_prj/debian.tf
+++ Day_1/debian_prj/debian.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "second_project" {
   ami           = "ami-073f149603898e301" #Amazon Linux 2023
   provider      = aws.lodon
-  instance_type = "t3.micro"
+  instance_type = var.ec2_instance_type
   subnet_id     = "subnet-05916c92c0c047eda"
   tags = {
     Name = "Terraform_Example"
