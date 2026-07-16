3aa9137b5aafb65ed084f30a69eee455daa3a4d9
Andre Araujo <andredearaujo2007@gmail.com>
2026-07-16 07:17:17 +0100

    Added a line called provider to identity what region the instance needs to execute

--- Day_1/debian_prj/debian.tf
+++ Day_1/debian_prj/debian.tf
@@ -1,5 +1,6 @@
 resource "aws_instance" "second_project" {
   ami           = "ami-073f149603898e301" #Amazon Linux 2023
+  provider      = aws.lodon
   instance_type = "t3.micro"
   subnet_id     = "subnet-05916c92c0c047eda"
   tags = {
