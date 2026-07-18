f7330703b07e6855e17317d26cf7a8684707fc94
Andre de Araujo <andredearaujo2007@gmail.com>
2026-07-18 16:31:47 +0100

    +Added in line 19 more one security Group called Network  +Added in line 14 Enable assign Public IP automatic

--- Day_1/Kubernetes/criacao_cluster.tf
+++ Day_1/Kubernetes/criacao_cluster.tf
@@ -3,14 +3,21 @@
 #Data 16/07/2026 (Thursday)
 
 resource "aws_instance" "cluster_kubernetes" {
-    count   = 1 #Here I'm specify how many instance I want to raise it 
+    #count   = 1 #Here I'm specify how many instance I want to raise it 
     provider = aws.london # Region where the aws will be raise
     instance_type = var.ec2_instance_type #Type Instance in this case T3.medium
     ami = var.image["eu-west-2"] #Image Instance
     subnet_id = var.subnet_id #Subnet where will be used
     key_name = var.keypair #Name keypair to acess via ssh
+
+    #Enable assign Public IP automatic
+    associate_public_ip_address = true
     
-    vpc_security_group_ids = ["sg-0663f337f58451693"] #This is already exist a security group defined
+    #This is already exist a security group defined
+    vpc_security_group_ids = [
+        "sg-0663f337f58451693", 
+        "sg-0af372fc867e0d109"
+        ] 
 
    tags = {
     Name = "kubernetes_instance-$count.index + 1}" #Name will be in tags
