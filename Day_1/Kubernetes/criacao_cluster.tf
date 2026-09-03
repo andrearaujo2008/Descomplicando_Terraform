5388862523406d085f2b345ca1a89c7075fa1fe0
Andre Araujo <andredearaujo2007@gmail.com>
2026-09-03 10:50:25 +0100

    wip: add baseline that assign ip elastic for each instance to raise

--- Day_1/Kubernetes/criacao_cluster.tf
+++ Day_1/Kubernetes/criacao_cluster.tf
@@ -2,8 +2,10 @@
 #Developed by André de Araujo 
 #Data 16/07/2026 (Thursday)
 
+
+#Instance AWS how will create to kubernetes
 resource "aws_instance" "cluster_kubernetes" {
-  count         = 1                      #Here I'm specify how many instance I want to raise it 
+  count         = 2                      #Here I'm specify how many instance I want to raise it 
   provider      = aws.london             # Region where the aws will be raise
   instance_type = var.ec2_instance_type  #Type Instance in this case T3.medium
   ami           = var.image["eu-west-2"] #Image Instance
@@ -21,6 +23,19 @@ resource "aws_instance" "cluster_kubernetes" {
 
   tags = {
     Name = "kubernetes_instance-${count.index + 1}" #Name will be in tags
+  }
+
+}
+
+#Elastic IP
+resource "aws_eip" "base" {
+  count    = 2
+  provider = aws.london # Region where the aws will be assign these ips
+  instance = aws_instance.cluster_kubernetes[count.index].id
+  domain   = "vpc"
+
+  tags = {
+    Name = "kube_eip"
   }
 
 }
